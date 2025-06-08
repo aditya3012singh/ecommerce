5e8895914aeedb5dc743f09889086c24b271e193
aviralMadhvan24 <aviralmadhvan@gmail.com>
2025-06-08 22:14:47 +0530

    Added product card component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,16 @@ import { Signup } from './pages/Signup'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signin } from './pages/Signin'
 import { Product } from './pages/Product'
-
+import  Home  from './pages/Home'
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <>
     <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup/>}/>
+          <Route path="/" element={<Home/>}/>
             
           <Route path="/signin" element={<Signin/>}/>
           <Route path="/Product" element={<Product/>}/>
